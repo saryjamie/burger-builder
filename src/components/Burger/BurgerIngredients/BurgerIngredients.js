cdf38c3567c654b82ba8ca7dbfbcd2baa8bd49ae
Sary Jamie <sary.jamie@gmail.com>
2021-01-30 15:32:25 -0800

    adding meet cheese and bacon to the switch statement

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -8,7 +8,25 @@ const burgerIngredients = (props) => {
       ingredient = <div className={classes.BreadBottom}></div>;
       break;
     case "bread-top":
-      ingredient = <div className={classes.BreadTop}></div>;
+      ingredient = (
+        <div className={classes.BreadTop}>
+          <div className={classes.seeds1}></div>
+          <div className={classes.seeds2}></div>
+        </div>
+      );
+      break;
+    case "meat":
+      ingredient = <div className={classes.Meat}></div>;
+      break;
+    case "cheese":
+      ingredient = <div className={classes.Cheese}></div>;
+      break;
+    case "salad":
+      ingredient = <div className={classes.Salad}></div>;
+      break;
+    case "bacon":
+      ingredient = <div className={classes.Baon}></div>;
+      break;
   }
 };
 
