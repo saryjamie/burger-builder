3bac750201c09c73fa5c8ad5e89cb0bfdd193718
Sary Jamie <sary.jamie@gmail.com>
2021-01-30 15:26:32 -0800

    using switch statment to add ingredients

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -1,5 +1,15 @@
 import React from "react";
+import classes from "./BurgerIngredient.css";
 
-const burgerIngredients = (props) => {};
+const burgerIngredients = (props) => {
+  let ingredient = null;
+  switch (props.type) {
+    case "bread-bottom":
+      ingredient = <div className={classes.BreadBottom}></div>;
+      break;
+    case "bread-top":
+      ingredient = <div className={classes.BreadTop}></div>;
+  }
+};
 
 export default burgerIngredients;
