58aa808f414d032e8747ce2887679f388d831b0b
Sary Jamie <sary.jamie@gmail.com>
2021-02-03 13:20:28 -0800

    fixed rendering issue with the burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,14 @@ import classes from "./Burger.css";
 import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
 
 const burger = (props) => {
-  <div className={classes.Burger}>
-    <BurgerIngredients type="bread-top" />
-    <BurgerIngredients type="bread-bottom" />
-    <BurgerIngredients type="cheese" />
-  </div>;
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredients type="bread-top" />
+      <BurgerIngredients type="cheese" />
+      <BurgerIngredients type="meat" />
+      <BurgerIngredients type="bread-bottom" />
+    </div>
+  );
 };
 
 export default burger;
--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import classes from "./BurgerIngredient.css";
+import classes from "./BurgerIngredients.css";
 import PropTypes from "prop-types";
 
 class BurgerIngredients extends Component {
