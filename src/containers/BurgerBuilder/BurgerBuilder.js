f789c17e440a3e7913559c7a68a861bb447f93ba
Sary Jamie <sary.jamie@gmail.com>
2021-02-04 11:18:08 -0800

    passing ingredients to the burger component tag in burgerBuilder class component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,7 @@ class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Control</div>
       </Aux>
     );
