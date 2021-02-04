9ba7503c23778d4f8a86bc7fe7526fa2df11121d
Sary Jamie <sary.jamie@gmail.com>
2021-02-04 11:16:37 -0800

    adding a state to burgerBuilder class copmponent

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meet: 2,
+    },
+  };
+
   render() {
     return (
       <Aux>
