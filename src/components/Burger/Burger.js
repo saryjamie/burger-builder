df5e839801f4ad07b142a9af4db26cb29a28957e
Sary Jamie <sary.jamie@gmail.com>
2021-02-05 01:35:03 -0800

    adding a console log to check the array of the ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -12,6 +12,7 @@ const burger = (props) => {
     .reduce((arr, el) => {
       return arr.concat(el);
     }, []);
+  console.log(transformedIngredients);
   if (transformedIngredients.length === 0) {
     transformedIngredients = <p>Please start adding ingredients</p>;
   }
