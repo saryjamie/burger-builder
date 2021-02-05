3c054efe8baadc6cba4a183b0a2c1c900250eb2a
Sary Jamie <sary.jamie@gmail.com>
2021-02-05 01:28:01 -0800

    adding a reduce method to the map method to transform it to another array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,15 @@ import classes from "./Burger.css";
 import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredients key={igKey + i} type={igKey} />;
-    });
-  });
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredients key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
   return (
     <div className={classes.Burger}>
       <BurgerIngredients type="bread-top" />
