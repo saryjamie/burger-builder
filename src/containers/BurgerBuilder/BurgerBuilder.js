f21b9e68655b47eb38f9966fe79d4936a0389637
Sary Jamie <sary.jamie@gmail.com>
2021-02-08 13:44:26 -0800

    typo

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,10 @@ import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 1,
-      meat: 2,
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
     },
   };
 
