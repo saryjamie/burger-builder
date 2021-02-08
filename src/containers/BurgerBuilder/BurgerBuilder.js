b42dcde8a83396712145dd2e0877d80a2033d6f0
Sary Jamie <sary.jamie@gmail.com>
2021-02-07 22:59:13 -0800

    updating the ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,9 +4,9 @@ import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 0,
-      bacon: 0,
-      cheese: 0,
+      salad: 1,
+      bacon: 1,
+      cheese: 1,
       meat: 2,
     },
   };
