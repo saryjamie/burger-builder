6db1c21e434ad4fc2432c935f583bca15de5edbe
Sary Jamie <sary.jamie@gmail.com>
2021-02-12 12:34:13 -0800

    output the ingredients dynamically by creating a const of ingredientSummary in OrderSummary component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Aux from "../../../hoc/Aux";
 
-const orderSummary = (props) => {};
+const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+    return (
+      <li>
+        {igKey} : {props.ingredients[igKey]} = 0
+      </li>
+    );
+  });
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>Your burger includes the following ingredients:</p>
+      <ul></ul>
+    </Aux>
+  );
+};
 
 export default orderSummary;
