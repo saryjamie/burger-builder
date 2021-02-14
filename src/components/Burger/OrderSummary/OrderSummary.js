f7a524ac24f6642b44244fe496cb89dda0b79c1c
Sary Jamie <sary.jamie@gmail.com>
2021-02-13 20:15:33 -0800

    adding a key to the li item in OrderSummary component to remove the error in the console

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,7 +4,7 @@ import Aux from "../../../hoc/Aux";
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
-      <li>
+      <li key={igKey}>
         <span style={{ textTransform: "capitalize" }}>{igKey}</span> :{" "}
         {props.ingredients[igKey]}
       </li>
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Modal.css";
 
 const modal = (props) => {
-  <div className={classes.Modal}>{props.children}</div>;
+  return <div className={classes.Modal}>{props.children}</div>;
 };
 
 export default modal;
