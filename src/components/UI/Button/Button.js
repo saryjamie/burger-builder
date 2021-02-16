f225221f4285d4bc72a96ed2176a85811f147522
Sary Jamie <sary.jamie@gmail.com>
2021-02-16 15:14:32 -0800

    changing the type of the className in button.js from an array into a srting by adding .join(' ') in the end of the array

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,10 @@ import React from "react";
 import classes from "./Button.css";
 
 const button = (props) => (
-  <button className={classes.Button} onClick={props.clicked}>
+  <button
+    className={[classes.Button, classes[props.btnType]].join(" ")}
+    onClick={props.clicked}
+  >
     {props.children}
   </button>
 );
