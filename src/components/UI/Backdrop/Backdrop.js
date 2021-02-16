1501a58ba49542bf974293403a5b642b9809df16
Sary Jamie <sary.jamie@gmail.com>
2021-02-15 16:11:12 -0800

    fixing props name from clicked to onClick

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -3,7 +3,7 @@ import classes from "./Backdrop.css";
 
 const backdrop = (props) =>
   props.show ? (
-    <div className={classes.Backdrop} clicked={props.clicked}></div>
+    <div className={classes.Backdrop} onClick={props.clicked}></div>
   ) : null;
 
 export default backdrop;
