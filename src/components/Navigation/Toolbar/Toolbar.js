2c9fe4031a174dc61d8bc1f787800981bdfc35d5
Sary Jamie <sary.jamie@gmail.com>
2021-02-18 11:22:07 -0800

    import classes into Toolbar.js

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,7 +1,12 @@
 import React from "react";
+import calsses from "./Toolbar.css";
 
 const toolbar = (props) => (
-
-)
+  <header className="classes.Toolbar">
+    <div>MENU</div>
+    <div>LOGO</div>
+    <nav>...</nav>
+  </header>
+);
 
 export default toolbar;
