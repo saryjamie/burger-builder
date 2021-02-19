f127fa3eb2af12a3039e8e8cb130091cf58463cb
Sary Jamie <sary.jamie@gmail.com>
2021-02-19 12:33:59 -0800

    adding Logo.css file and import it to Logo.js and pass it as a classNAme to the div and import burgerLogo to the Logo.js

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import burgerLogo from "../../assets/images/burger-logo.png";
+import classes from "./Logo/css";
+
+const logo = (logo) => (
+  <div className={classes.Logo}>
+    <img src={burgerLogo} alt="MyBurger" />
+  </div>
+);
+
+export default logo;
