576a9d80764043d883b898f370f956479dceae72
Sary Jamie <sary.jamie@gmail.com>
2021-02-20 01:15:30 -0800

    fixing Logo.css import into the Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import burgerLogo from "../../assets/images/burger-logo.png";
-import classes from "./Logo/css";
+import classes from "./Logo.css";
 
 const logo = (logo) => (
   <div className={classes.Logo}>
