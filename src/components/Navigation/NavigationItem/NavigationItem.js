73511a7183e8a98cdae576150eecf77367fc6163
Sary Jamie <sary.jamie@gmail.com>
2021-02-20 23:17:20 -0800

    setting up NavigationItem component function

---
+++ src/components/Navigation/NavigationItem/NavigationItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const navigationItem = (props) => (
+  <li>
+    <a href="">A LINK</a>
+  </li>
+);
+
+export default navigationItem;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-const navigationItems = (props) => (
-
-)
+const navigationItems = (props) => <ul></ul>;
 
 export default navigationItems;
