0b40001b7b6db517273ea62ac29f7eb981d12e9f
Sary Jamie <sary.jamie@gmail.com>
2021-02-22 16:28:47 -0800

    adjusting logo in the sideDrawer and toolbar dynamically

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,7 +7,9 @@ const sideDarwer = (props) => {
   //...
   return (
     <div className={classes.SideDrawer}>
-      <Logo />
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
       <nav>
         <NavigationItems />
       </nav>
