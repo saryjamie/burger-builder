0d0bfe172ff99cd8e6c8cc77472cf443a0f45a9c
Sary Jamie <sary.jamie@gmail.com>
2021-03-01 07:35:05 -0800

    import classes from DrawerToggle.css and pass it to the container div and add 3 divs inside which represent the lines for the burger menu

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,4 +1,11 @@
 import React from "react";
+import classes from "./DrawerToggle.css";
 
-const drawerToggle = (props) => <div onClick={props.clicked}>MENU</div>;
+const drawerToggle = (props) => (
+  <div className={classes.DrawerToggle} onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
 export default drawerToggle;
