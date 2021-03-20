be02ba118d9796b26b624c7f30b6d494659495d2
Sary Jamie <sary.jamie@gmail.com>
2021-03-20 00:37:20 -0700

    importing axios into BurgerBuilder.js component file and pass it to the purchaseContinue

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGRERDIENT_PRICES = {
   salad: 0.3,
@@ -76,7 +77,25 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continued!");
+    // alert("You continued!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Sary Jamie",
+        address: {
+          street: "123 Main St",
+          zipCode: "92101",
+          country: "USA",
+        },
+        email: "saryjamie@myweb.com",
+      },
+      deliveryMethod: "fast",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   render() {
