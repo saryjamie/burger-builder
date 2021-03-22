68669f4f1cd561434994d864fa7046791476e2be
Sary Jamie <sary.jamie@gmail.com>
2021-03-22 12:06:17 -0700

    creating a Spinner.css class and import it into Spinner.js component

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,5 +1,6 @@
 import React from "react";
+import classes from "./Spinner.css";
 
-const spinner =() => ();
+const spinner = () => <div className={classes.Loader}>Loading...</div>;
 
 export default spinner;
