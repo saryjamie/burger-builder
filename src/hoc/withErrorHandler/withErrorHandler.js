e66e555fd76bd556ba15aa8251bc9992d63af000
Sary Jamie <sary.jamie@gmail.com>
2021-03-25 11:22:40 -0700

    modifying withErrorHandler.js

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { Component} from "react";
 import Modal from "../../components/UI/Modal/Modal";
 import Aux from "../Aux/Aux";
 
-const withErrorHandler = (wrappedComponent) => {
-  return (props) => {
+const withErrorHandler = (wrappedComponent, axios) => {
+  return class extends Component {
+      componentDidMount(){
+          axios.interceptors.response.use(response)
+      }
+      render() {
+          return (
+            <Aux>
+            <Modal show>Something didn't work!</Modal>
+            <wrappedComponent {...this.props} />
+          </Aux>
+          )
+      }
+  } (props) => {
     return (
-      <Aux>
-        <Modal show>Something didn't work!</Modal>
-        <wrappedComponent {...props} />
-      </Aux>
+
     );
   };
 };
