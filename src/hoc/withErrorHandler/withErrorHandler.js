c7e943370f2eb060afe56c48f3d91e140df6d469
Sary Jamie <sary.jamie@gmail.com>
2021-03-27 22:31:02 -0700

    getting rid of the extra code thats left from transforming funciton component into a class component in the withErrorHandler.js file. fixing a typo of wrappedComponent props to WrappedComponent to use it as a component tag

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,38 +1,37 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import Modal from "../../components/UI/Modal/Modal";
 import Aux from "../Aux/Aux";
 
-const withErrorHandler = (wrappedComponent, axios) => {
+const withErrorHandler = (WrappedComponent, axios) => {
   return class extends Component {
-      state = {
-          error: null
-      }
-      componentDidMount(){
-          axios.interceptors.request.use(req => {
-              this.setState({error: null})
-              return req
-            
-          })
-          axios.interceptors.response.use(res => res, error => {
-            this.setState({error: error})
-          })
-      }
-      errorConfirmedHandler = () => {
-          this.setState({error: null})
-      }
-      render() {
-          return (
-            <Aux>
-            <Modal show={this.state.error}
-            clicked={this.errorCofirmedHandler}>{this.state.error ? this.state.error.message : null}</Modal>
-            <wrappedComponent {...this.props} />
-          </Aux>
-          )
-      }
-  } (props) => {
-    return (
-
-    );
+    state = {
+      error: null,
+    };
+    componentDidMount() {
+      axios.interceptors.request.use((req) => {
+        this.setState({ error: null });
+        return req;
+      });
+      axios.interceptors.response.use(
+        (res) => res,
+        (error) => {
+          this.setState({ error: error });
+        }
+      );
+    }
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+    render() {
+      return (
+        <Aux>
+          <Modal show={this.state.error} clicked={this.errorCofirmedHandler}>
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
   };
 };
 
