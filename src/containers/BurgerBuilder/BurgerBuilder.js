dbf71440026e5790d46a9d25b1043617df2e57c1
Sary Jamie <sary.jamie@gmail.com>
2021-04-02 00:34:53 -0700

    fixing url link in the axios method

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,7 +28,7 @@ class BurgerBuilder extends Component {
   componentDidMount() {
     axios
       .get(
-        "https://my-burger-react-11eaa-default-rtdb.firebaseio.com/ingredients"
+        "https://my-burger-react-11eaa-default-rtdb.firebaseio.com/ingredients.json"
       )
       .then((response) => {
         this.setState({ ingredients: response.data });
