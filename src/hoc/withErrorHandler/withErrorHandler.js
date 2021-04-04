f1f2f65d7857df069d9d942d75e1469cc38d0f38
Sary Jamie <sary.jamie@gmail.com>
2021-04-04 02:05:36 -0700

    adding an eject method to the axios request and response

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -20,7 +20,10 @@ const withErrorHandler = (WrappedComponent, axios) => {
       );
     }
 
-    componentWillUnmount() {}
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
 
     errorConfirmedHandler = () => {
       this.setState({ error: null });
