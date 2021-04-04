edb85e847059c3a401489a5298f841e1ebf20eff
Sary Jamie <sary.jamie@gmail.com>
2021-04-04 02:03:16 -0700

    storing interceptors into a this.reqOrresInterseptor

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,11 +8,11 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null,
     };
     componentWillMount() {
-      axios.interceptors.request.use((req) => {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         (res) => res,
         (error) => {
           this.setState({ error: error });
